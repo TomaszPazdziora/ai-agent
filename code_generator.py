28fc08688a684a39fa8120db37cbf56d639083b8
TomaszPazdziora <tomekpazdziora@gmail.com>
2024-11-28 14:27:31 +0100

    rewrite to english version

--- code_generator.py
+++ code_generator.py
@@ -30,11 +30,9 @@ def write_to_file(content: str, file_path: str) -> None:
 if __name__ == "__main__":
     tools = [write_to_file]
 
-    # query = "Create example C project in test directory. You shoud create a main.c file two examples .c/.h files and a Makefile with build and execution instructions."
-
-    instructions = "Jesteś pomocnym asystentem programisty. Twoim zadaniem jest tworzenie szablonów projektów programistycznych. Użytkownik będzie podawać Ci instrukcje. Powinieneś stworzyć każdy z plików podanych przez użytkownika oraz wypełnić go kodem działającym zgodnie z opisem użytkownika. Do tworzenia plików, wpisywania zawartości do plików oraz odczytywania zawartości z plików wykorzystaj powierzone Ci narzędzia. Po wykonaniu zadania opisz jakie pliki utworzyłeś oraz streść bardzo krótko co się w nich znajduje. W następnym kroku przedstaw strukturę utworzonego projektu. Oraz utwórz plik readme.md w którym opiszesz w jaki sposób uruchomić projekt."
+    instructions = "You are a helpful programming assistant. Your task is to create templates for programming projects. The user will provide you with instructions. You should create each of the files mentioned by the user and fill them with code that works according to the user's description. Use the 'write_to_file' tool to create the file and write the content to the created file. After completing the task, present the generated project file structure."
     
-    query = "Stwórz mi stronę internetową. Chciałbym żebyś zrobił to w pythonie. Wykorzystaj framework Flask do napisania backendu aplikacji. Stwórz główną stronę i parę odnośników do innych zakładek. Frontend powinien być napisany w htmlu i css. Chciałbym aby strona miała kilka przycisków, pól do wypełnienia oraz checkboxów. Cały projekt umieść w folderze 'web'"
+    query = "Create a website for me. I would like you to do it in Python. Use the Flask framework to write the application's backend. Create home, about and careers pages. The frontend should be written in HTML and CSS. I would like the page to have several buttons, input fields, and checkboxes. Place the entire project in a folder called 'web'. I like shrek."
     
     logger.info(f"User input: {query}")
     coder = Agent(tools=tools, master_prompt=instructions)
